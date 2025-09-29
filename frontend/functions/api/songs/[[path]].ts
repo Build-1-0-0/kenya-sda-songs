67d0c5fb77ae5e7e468c9ff098515aa5896a4131
Build-1-0-0 <africancontent807@gmail.com>
2025-09-30 01:21:50 +0300

    Create [[path]].ts

---
+++ frontend/functions/api/songs/[[path]].ts
@@ -0,0 +1,8 @@
+export const onRequest: PagesFunction = async ({ request }) => {
+  const workerUrl = 'https://sda-api.africancontent807.workers.dev' + new URL(request.url).pathname;
+  return fetch(workerUrl, {
+    method: request.method,
+    headers: request.headers,
+    body: request.body,
+  });
+};
