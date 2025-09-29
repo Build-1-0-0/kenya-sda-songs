ac5a7302b884fb01fd0d2d82d93107d53d286368
Build-1-0-0 <africancontent807@gmail.com>
2025-09-30 01:55:39 +0300

    Update [[path]].ts

--- frontend/functions/api/[[path]].ts
+++ frontend/functions/api/[[path]].ts
@@ -1,15 +1,8 @@
-import type { PagesFunction } from '@cloudflare/workers-types';
-
-export const onRequest: PagesFunction = async ({ request }) => {
+export async function onRequest({ request }) {
   const workerUrl = 'https://sda-api.africancontent807.workers.dev' + new URL(request.url).pathname;
-  const response = await fetch(workerUrl, {
+  return fetch(workerUrl, {
     method: request.method,
-    headers: request.headers as HeadersInit,
+    headers: request.headers,
     body: request.body,
   });
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers: response.headers as HeadersInit,
-  });
-};
+}
