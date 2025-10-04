36f7ac6333b852381cc01602b6a37b202f334aa0
Build-1-0-0 <africancontent807@gmail.com>
2025-10-04 23:34:16 +0300

    Update [[path]].ts

--- frontend/functions/api/[[path]].ts
+++ frontend/functions/api/[[path]].ts
@@ -1,11 +1,5 @@
-export async function onRequest(context) {
+export async function onRequest(context: any) {
   const response = await fetch("https://sda-api.africancontent807.workers.dev/songs.json");
   const data = await response.json();
-
-  return new Response(JSON.stringify(data), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    }
-  });
+  return new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" } });
 }
