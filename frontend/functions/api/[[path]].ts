837bd621b19f651993e65e72d723f0b047e27e31
Build-1-0-0 <africancontent807@gmail.com>
2025-10-04 23:42:37 +0300

    Delete frontend/functions/api/[[path]].ts

--- frontend/functions/api/[[path]].ts
+++
@@ -1,7 +0,0 @@
-import type { EventContext } from '@cloudflare/workers-types';
-
-export async function onRequest(context: EventContext) {
-  const response = await fetch("https://sda-api.africancontent807.workers.dev/songs.json");
-  const data = await response.json();
-  return new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" } });
-}
