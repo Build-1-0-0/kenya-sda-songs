9dcac9c1c56661d238a0d641ce1b505862466c5f
Build-1-0-0 <africancontent807@gmail.com>
2025-10-04 23:38:30 +0300

    Update [[path]].ts

--- frontend/functions/api/[[path]].ts
+++ frontend/functions/api/[[path]].ts
@@ -1,4 +1,6 @@
-export async function onRequest(context: any) {
+import type { EventContext } from '@cloudflare/workers-types';
+
+export async function onRequest(context: EventContext) {
   const response = await fetch("https://sda-api.africancontent807.workers.dev/songs.json");
   const data = await response.json();
   return new Response(JSON.stringify(data), { headers: { "Content-Type": "application/json" } });
